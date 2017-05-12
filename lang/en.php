1ccd3c3f659aeadf787278200a97f00a16ae5991
Florestan Bredow <daiko@daiko.fr>
2017-05-12 10:35:12 +0200

    Complete les traductions

--- lang/en.php
+++ lang/en.php
@@ -3,4 +3,13 @@
 $etresTraductions = array(
     'hat' => 'A European network to train the sustainable development and environmental education stakeholders supporting the ecological transition through citizens’ initiatives. <a class="plus" href="?wiki=IntroductionEn">+</a>',
     'punchLine' => 'Supporting the ecological transition through citizens’ initiatives',
+    'partners' => 'Partners',
+    'resources' => 'Resources',
+    'allNews' => 'All news',
+    'yourName' => 'Surname and Name',
+    'email' => 'eMail',
+    'mailSubject' => 'Mail subject',
+    'mailMessage' => 'Your message',
+    'send' => 'Send',
+    'question' => 'A question ?',
 );
