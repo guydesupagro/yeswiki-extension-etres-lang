5c065397d4aef967439924036e973b6aa1e79394
guydesupagro <guydesupagro@gmail.com>
2017-06-19 10:35:46 +0200

    Update fr.php

--- lang/fr.php
+++ lang/fr.php
@@ -1,7 +1,7 @@
 <?php
 
 $etresTraductions = array(
-    'hat' => 'Un réseau européen pour la formation des acteurs en éducation à l’environnement et au développement durable (EEDD) accompagnant les dynamiques citoyennes de transition écologique. <a class="plus" href="?wiki=IntroductionFr">+</a>',
+    'hat' => 'Un réseau européen pour la formation des acteurs en éducation à l’environnement et au développement durable accompagnant les dynamiques citoyennes de transition écologique. <a class="plus" href="?wiki=IntroductionFr">+</a>',
     'punchLine' => 'Accompagner les dynamiques citoyennes de transition écologique',
     'partners' => 'Partenaires',
     'resources' => 'Ressources',
