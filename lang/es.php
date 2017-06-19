1f32c96c4fed56ee5119644f32d01bc8aaebcc4f
guydesupagro <guydesupagro@gmail.com>
2017-06-19 10:37:12 +0200

    Update es.php

--- lang/es.php
+++ lang/es.php
@@ -1,7 +1,7 @@
 <?php
 
 $etresTraductions = array(
-    'hat' => 'Una red europea para la formación de actores en educación ambiental y desarrollo sostenible (EADS) que acompañen dinámicas ciudadanas de transición ecológica. <a class="plus" href="?wiki=IntroductionEs">+</a>',
+    'hat' => 'Una red europea para la formación de actores en educación ambiental y desarrollo sostenible que acompañen dinámicas ciudadanas de transición ecológica. <a class="plus" href="?wiki=IntroductionEs">+</a>',
     'punchLine' => 'Acompañar las dinámicas ciudadanas hacia la transición ecológica',
     'partners' => 'Socios',
     'resources' => 'Recursos',
